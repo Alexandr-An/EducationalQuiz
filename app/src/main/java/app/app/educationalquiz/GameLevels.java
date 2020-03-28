9ea433746d695e7bb6c6e48f789678193bd8b00b
AlAndreev92 <brabusmax@mail.ru>
2020-03-28 19:02:04 +0300

    системная кнопка назад

--- app/src/main/java/app/app/educationalquiz/GameLevels.java
+++ app/src/main/java/app/app/educationalquiz/GameLevels.java
@@ -1,10 +1,16 @@
 package app.app.educationalquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.reflect.GenericArrayType;
 
 public class GameLevels extends AppCompatActivity {
 
@@ -15,5 +21,28 @@ public class GameLevels extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); // Убрать строку состояния.
+        Button buttonBack = (Button)findViewById(R.id.button_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // команда для кнопки назад
+                //начало конструкции
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);finish();
+                }catch (Exception e){}
+                //конец конструкции
+            }
+        });
     }
+    //системная кнопка назад начало
+    @Override
+   public void onBackPressed(){
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);finish();
+        }catch (Exception e){}
+    }
+
+    //системная кнопка назад конец
 }
