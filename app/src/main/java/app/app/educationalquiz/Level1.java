9d549ab63f9618e548bb0081cebce9db03d9706a
AlAndreev92 <brabusmax@mail.ru>
2020-03-29 12:47:28 +0300

    кнопка закрыть и продолжить диалоговое окно

--- app/src/main/java/app/app/educationalquiz/Level1.java
+++ app/src/main/java/app/app/educationalquiz/Level1.java
@@ -2,12 +2,23 @@ package app.app.educationalquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
 
 public class Level1 extends AppCompatActivity {
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +35,44 @@ public class Level1 extends AppCompatActivity {
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         //развернуть игру на весь экран начало
+
+
+        //Вызов диалогового окна в начале первого уровня
+        dialog = new Dialog(this);// новое диалоговое окно
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);//скрыть загооловок стандартный
+        dialog.setContentView(R.layout.previewdialog);// путь к маекету диалогового окна
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//прозрачныйыы фон диалогового окна
+        dialog.setCancelable(false);//окно нельзя закрытть кнопкой назад
+        //кнопка закрытия диалогового окна начало
+        TextView btnclose = (TextView)dialog.findViewById(R.id.button_close);
+        btnclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //обработка нажатия кнопки начало
+                try {
+                    //вернуться назад к выбору уровня начало
+                    Intent intent = new Intent(Level1.this, GameLevels.class);//намерение для перехода
+                    startActivity(intent);//старт намерения
+                    finish();//закрыть класс
+                    //вернуться назад к выбору уровня конец
+                }catch (Exception e){}
+                dialog.dismiss();//закрываем диалоговое окно
+                //обработка нажатия кнопки конец
+            }
+        });
+        //кнопка закрытия диалогового окна конец
+
+        //кнопка продожить начло
+        Button btncontinue = (Button)dialog.findViewById(R.id.buttoncontinue);
+        btncontinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();// закрываем диалоговое окно/
+            }
+        });
+
+        //кнопка продожить конец
+
+        dialog.show();//показать диалоговое окно
     }
 }
